53aaf1841a1ef24cc8a56232bb3f67aa0c023d63
judeserem <80768824+judeserem@users.noreply.github.com>
2024-07-17 12:31:05 +0300

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,17 @@
+public class operators {
+    public static void main(String[] args) {
+        //mathematical operation
+        //area of square
+       /*  int length=12;
+        int area=length*length;
+.out        System.println("the area equals to "+area); */
+       /* int length=6;
+        int width=6;
+        int area=length*width;
+        System.out.println("the area the rectangle of the  equals to "+area); */ 
+        int fathersage=36;
+        int childsage=6;
+        int agediference=fathersage-childsage;
+        System.out.println("the father is older than the child by "+agediference);
+    }    
+}
